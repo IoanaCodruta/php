410f76faaf2e2b5b5d92c9c295bb04d3b72a8a05
IoanaCodruta <ioanacodrutaopris@gmail.com>
2023-05-04 17:30:49 +0200

    updated 04/05

---
+++ 6. ejercicio-echo.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "<h2> PHP es divertido! </h2>";
+
+echo "Hola mundo! <br>";
+
+echo "Estoy a punto de aprender PHP! <br>";
+
+echo "Esta", " cadena de texto ", "se ha", "
+creado ", " con multiples parámetros.";
+
+echo '<br><a href="6. ejercicio-print.php">ejercicio de print()</a>';
+
+?>
+<!-- Si finalizamos el código php podriamos integrar HTML -->
+<br>
+<a href="6. ejercicio-print.php">ejercicio de print()</a>
